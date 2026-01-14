b4db3772847afce0ad3044ec03a09c8a0e2d6d51
sandimorea953-ops <sandimorea953@gmail.com>
2026-01-14 23:22:21 +0800

    Update login-script.js
    Mengganti komentar sesuai dengan format JSdocs

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -1,25 +1,13 @@
-/*
- * Catatan:
- * Gunakan fungsi goToHome() untuk menampilkan halaman home
- * Gunakan fungsi goToLogin() untuk menampilkan halaman login
- * Gunakan fungsi showPopUp() untuk menampilkan pop up error
- */
-
+/** @module Login-Script */
+/** 
+* Membuat variabel loginFormElement untuk tampilan form.
+* @constant {HTMLElement}
+*/
 const loginFormElement = document.querySelector('#loginForm');
+ 
+ 
+/** 
+* Membuat variabel inputEmailElement untuk tampilan input email.
+* @constant {HTMLElement}
+*/
 const inputEmailElement = document.querySelector('#inputEmail');
-const inputPasswordElement = document.querySelector('#inputPassword');
-
-const expectedEmail = 'admin@dicoding.com';
-const expectedPassword = 'superpassword';
-
-loginFormElement.addEventListener('submit', function(event) {
-  event.preventDefault();
-
-  const email = inputEmailElement.value;
-  const password = inputPasswordElement.value;
-  if (email == expectedEmail && password == expectedPassword) {
-    goToHome();
-  } else {
-    showPopUp();
-  }
-});
